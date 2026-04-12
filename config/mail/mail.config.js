f89c94cb227191a8f5e7d620e6ebdd1f625a04ca
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-04-12 15:23:29 -0400

    feat:mail chor

--- config/mail/mail.config.js
+++ config/mail/mail.config.js
@@ -1,15 +1,14 @@
 const nodemailer = require('nodemailer');
 
 const gmailtransporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+    host: 'smtp.gmail.com',
   port: 465,
-  secure: true, // use true only for 465
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   connectionTimeout: 10000,
-  greetingTimeout: 10000,
   socketTimeout: 10000,
 });
 
