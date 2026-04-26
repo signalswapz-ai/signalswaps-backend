6083b29dd9c405742ca1f96a0bfdcbcafea85670
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-04-26 14:32:15 -0400

    feat:spot trade LIST Added

--- routes/withdraw.routes.js
+++ routes/withdraw.routes.js
@@ -3,9 +3,9 @@ const withdrawController = require('../controllers/withdraw.controller');
 const { authenticate } = require('../middleware/auth');
 const { apiRateLimiter } = require('../middleware/apiRateLimiter');
 const router = express.Router();
-
+// apiRateLimiter(),
 // routes
-router.post('/create',authenticate, apiRateLimiter(), withdrawController.createWithdraw);
+router.post('/create',authenticate,  withdrawController.createWithdraw);
 router.post('/list', authenticate,  withdrawController.getWithdrawsByUser);
 
 module.exports = router;
