89801160214a752dea04e5e65f23f1199e4bfe63
gopalrakshita2-code <gopalrakshita2@gmail.com>
2026-05-11 15:32:27 -0400

    feat:commodity-trade api service v1

--- routes/commodityTrade.routes.js
+++ routes/commodityTrade.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const commodityTradeController = require('../controllers/commodityTrade.controller');
 
 router.post('/create', commodityTradeController.createCommodityTrade);
-// router.post('/list', commodityTradeController.getCommodityTradesByUser);
+router.post('/list', commodityTradeController.getCommodityTradesByUser);
 
 module.exports = router;
